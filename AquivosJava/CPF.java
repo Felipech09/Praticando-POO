08a0e5697176ca9ec57bd386a576d32369959611
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-12-01 16:18:52 -0300

    Continuação do código

--- AquivosJava/CPF.java
+++ AquivosJava/CPF.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class CPF
 
 
-  // aqui ficará guardado de modo privado a lista de CPFs
-private string<lit> fila
+  // Atributo que guardará de modo privado a lista de CPFs
+private List<String> fila;
+
+// COnstrutor de classe
+public CPF() {
+  this.fila = new ArrayList<>();
+}
+
+public void inserirCPF(String cpf) {
+  if (fila.contains(cpf)) {
+  System.out.println("CPF já cadastrado!");
+  } else {
+  System.out.println("CPF inserido om sucesso");
+  }
+}
+
+public void listarCPF (String senha) {
+  if (senha.equals("redes2025")) {
+    if (fila.isEmpty()) {
+      System.out.println("Fila vazia!");
+    } else {
+      System.out.println("CPF na fila!");
+      for (String cpf : fila) {
+        System.out.println(cpf);
+      }
+    }
+  }
+}
