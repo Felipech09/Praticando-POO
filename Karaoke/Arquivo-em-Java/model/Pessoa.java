c9b132e51dabdbf7ff18a1de97dda1bd0d2d2bb3
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-12-22 14:05:06 -0300

    Código de pessoas

--- Karaoke/Arquivo-em-Java/model/Pessoa.java
+++ Karaoke/Arquivo-em-Java/model/Pessoa.java
@@ -1,3 +1,29 @@
 public class Pessoa {
-  
+  private static int contadorID = 1;
+  private int id;
+  private String nome;
+  private String musica;
+
+  public Pessoa(String nome, String musica) {
+    this.id = contadorID++;
+    this.nome = nome;
+    this.musica = musica;
+  }
+
+  public int getID(){{
+    return id;
+  }
+  public String getNome(){
+    return Nome;
+  }
+ public String getMusica(){
+   return Musica;
+ }
+  public void String setMusica(){
+    this.musica = musica;
+  }
+  @Override 
+  public String toString() { 
+    return "ID: " + id + " | Nome: " + nome + " | Música: " + musica; 
+  }
 }
