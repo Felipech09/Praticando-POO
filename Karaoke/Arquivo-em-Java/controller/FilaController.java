72ecb47be0e5af93bf2b16d2a6758250de276acd
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-12-23 15:44:28 -0300

    adicionando os métodos adicionar pessoa e remover pessoa
    Implemented methods to add and remove people from the queue.

--- Karaoke/Arquivo-em-Java/controller/FilaController.java
+++ Karaoke/Arquivo-em-Java/controller/FilaController.java
@@ -1,3 +1,27 @@
-public class Fila Controller {
+import java.util.Queue; 
+import java.util.LinkedList; 
+
+public class FilaController { 
+  private Queue<Pessoa> fila; 
+  private int totalCantaram;
   
+  public FilaController() { 
+    fila = new LinkedList<>(); totalCantaram = 0; 
+  }
+
+  // adicionar
+public void adicionarPessoa(String nome, String musica) { 
+  Pessoa p = new Pessoa(nome, musica); 
+  fila.add(p); 
+  System.out.println("Pessoa adicionada à fila!"); 
 }
+
+  // remover
+  public void removerPessoa(int id) { 
+    boolean removido = fila.removeIf(p -> p.getId() == id); 
+    if (removido) { 
+      System.out.println("Pessoa removida da fila.");
+      } else { 
+      System.out.println("ID não encontrado."); 
+    } 
+  }
