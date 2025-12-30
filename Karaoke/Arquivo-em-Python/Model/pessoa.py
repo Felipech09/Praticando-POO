56542dc2a69217a567a4576cf81ec07431546dce
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-12-30 11:53:45 -0300

    Implement Pessoa class with attributes and methods

---
+++ Karaoke/Arquivo-em-Python/Model/pessoa.py
@@ -0,0 +1,24 @@
+class Pessoa:
+  __contador_id = 1
+
+def __init__(self, nome, musica):
+  self.__id = pessoa.__contador_id
+  Pessoa.__contador_id += 1
+  self.__nome = nome
+  self.__ = nome
+  self.__musica = musica
+
+def get_id(self):
+  return self.__id
+
+def get_nome(self):
+  return self.__nome
+
+def get_musica(self):
+  return self.__musica
+
+def set_musica(self, musica): 
+  self.__ musica = musica
+
+def __str__(self):
+  return f"ID: {self.__id} | Nome: {self.__nome} | Música: {self.__musica}"
