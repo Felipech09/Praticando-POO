21dde7f55d2ef57227bc5e205822e44a4fc5f1c1
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-12-30 00:15:02 -0300

    Corrigindo erros

--- Karaoke/Arquivo-em-Java/controller/FilaController.java
+++ Karaoke/Arquivo-em-Java/controller/FilaController.java
@@ -1,56 +1,54 @@
-import java.util.Queue; 
-import java.util.LinkedList; 
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class FilaController { 
-  private Queue<Pessoa> fila; 
-}
+public class FilaController {
+    private FilaKaraoke filaKaraoke;
 
-  public FilaController() { 
-    fila = new LinkedList<>();
-  }
-
-  // adicionar
-public void adicionarPessoa(String nome, String musica) { 
-  Pessoa p = new Pessoa(nome, musica); 
-  fila.add(p); 
-  System.out.println("Pessoa adicionada à fila!"); 
-}
-
-  // remover
-  public void removerPessoa(int id) { 
-    boolean removido = fila.removeIf(p -> p.getId() == id); 
-    if (removido) { 
-      System.out.println("Pessoa removida da fila.");
-      } else { 
-      System.out.println("ID não encontrado."); 
-    } 
-  }
-
-public void aletararMusica(int id, String novaMusica) {
-  for (Pessoa p : fila) {
-    if (p.getId() == id) {
-      p.setMusica(novaMusica);
-      System.out.println("Música alterada com sucesso!");
-      return;
+    public FilaController() {
+        this.filaKaraoke = new FilaKaraoke();
     }
-  }
-  System.out.println("ID não encontrado");
-}
 
-public void chamarProxima(){
-  Pessoa p = fila.poll();
-  if (p != null) {
-    System.out.println("Chamando agora: " + p.getNome() + " para cantar [" + p.getMusica() + "]);
-  } else {
-    System.out.printon ("A fila está vazia!");
-  }
-}
+    public void adicionarPessoa(String nome, String musica) {
+        Pessoa p = new Pessoa(nome, musica);
+        filaKaraoke.adicionarPessoa(p);
+        System.out.println("Pessoa adicionada à fila!");
+    }
 
-public void mostrarFila(){
-  if (fila.isempty()){
-    System.out.println("Fila vazia!");
-  } else {
-    System.out.println (" \n --- A fila está organizada da seguinte maneira: --- \n");
-    fila.forEach(System.out: :println);
-  }
+    public void removerPessoa(int id) {
+        boolean removido = filaKaraoke.removerPessoa(id);
+        if (removido) {
+            System.out.println("Pessoa removida da fila.");
+        } else {
+            System.out.println("ID não encontrado.");
+        }
+    }
+
+    public void alterarMusica(int id, String novaMusica) {
+        boolean alterado = filaKaraoke.alterarMusica(id, novaMusica);
+        if (alterado) {
+            System.out.println("Música alterada com sucesso!");
+        } else {
+            System.out.println("ID não encontrado.");
+        }
+    }
+
+    public void chamarProxima() {
+        Pessoa p = filaKaraoke.chamarProxima();
+        if (p != null) {
+            System.out.println("Chamando agora: " + p.getNome() + " para cantar '" + p.getMusica() + "'");
+        } else {
+            System.out.println("A fila está vazia!");
+        }
+    }
+
+    public void mostrarFila() {
+        if (filaKaraoke.isEmpty()) {
+            System.out.println("Fila vazia!");
+        } else {
+            System.out.println("\n--- A fila está organizada da seguinte maneira: ---");
+            for (Pessoa p : filaKaraoke.getFila()) {
+                System.out.println(p);
+            }
+        }
+    }
 }
