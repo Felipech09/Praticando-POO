c1fe1fbe696f452fa319fc67e17e33a27ce1a0c8
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-12-30 14:14:51 -0300

    Implement main function for karaoke system

---
+++ Karaoke/Arquivo-em-Python/View/main.py
@@ -0,0 +1,42 @@
+def main():
+  controller = FilaController()
+
+while True:
+  print("\n ========= KARAOKÊ =========")
+  print("1 - Adicionar pessosa")
+  print("2 - Remover pessoa")
+  print("3 - Alterar música")
+  print("4 - Chamar próxima pessoa")
+  print("5 - Mostrar fila")
+  print("0 - Sair")
+
+opcao = input ("Escolha uma opção: ")
+
+if opcao == "1":
+  nome = input("Nome: ")
+  musica = input ("Música; ")
+  controller.adicionar_pessoa(nome, musica)
+elif opcao == "2":
+  try:
+    id_rem = int(input("ID para remover: "))
+    controller.remover_pessoa(id_rem)
+  except ValueError:
+    print("Por favor, digite um ID numérico válido.")
+elif opcao == "3":
+  try:
+    id_alt = int(input("ID para alterar a música:"))
+    nome_musica = input ("Nova música:")
+    controller.alterar_musica(id_alt, nova_musica)
+  except ValueError:
+      print("Por favor, digite um ID numérico válido.")
+elif opcao == "4":
+  controller.chamar_proxima()
+elif opcao == "5":
+  controler.mostrar_fila()
+elif opcao == "0":
+  print("Encerrando o sistema...")
+  break
+else: print("Opção inválida!")
+
+if __name__ == "__main__":
+  main()
