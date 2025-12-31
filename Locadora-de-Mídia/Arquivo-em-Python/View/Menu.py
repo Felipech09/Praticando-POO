dd93b682c9d185c97f4d5cbdfa49d25c2a68e9e3
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-12-31 12:04:47 -0300

    Add MenuView class to display menu options

---
+++ Locadora-de-Mídia/Arquivo-em-Python/View/Menu.py
@@ -0,0 +1,11 @@
+class MenuView:
+    @staticmethod
+    def mostrar():
+      print("\n === Locadora de mídia ===")
+      print("1 - Cadastrar cliente")
+      print("2 - Listar clientes")
+      print("3 - Cadastrar mídia")
+      print("4 - Listar mídias")
+      print("5 - Abrir locação")
+      print("6 - Encerrar locação")
+      print("0 - Sair")
