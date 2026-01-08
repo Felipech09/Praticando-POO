f4c574331f7760f04478ffc6a2f74cc7eb6a3cbf
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2026-01-07 21:54:58 -0300

    Update pessoa.py
    Corrigindo erro de inicialização dos ID

--- Karaoke/Arquivo-em-Python/Model/pessoa.py
+++ Karaoke/Arquivo-em-Python/Model/pessoa.py
@@ -1,24 +1,23 @@
 class Pessoa:
-  __contador_id = 1
+    __contador_id = 1
 
-def __init__(self, nome, musica):
-  self.__id = pessoa.__contador_id
-  Pessoa.__contador_id += 1
-  self.__nome = nome
-  self.__ = nome
-  self.__musica = musica
+    def __init__(self, nome, musica):
+        self.__id = Pessoa.__contador_id
+        Pessoa.__contador_id += 1
+        self.__nome = nome
+        self.__musica = musica
 
-def get_id(self):
-  return self.__id
+    def get_id(self):
+        return self.__id
 
-def get_nome(self):
-  return self.__nome
+    def get_nome(self):
+        return self.__nome
 
-def get_musica(self):
-  return self.__musica
+    def get_musica(self):
+        return self.__musica
 
-def set_musica(self, musica): 
-  self.__ musica = musica
+    def set_musica(self, musica): 
+        self.__musica = musica
 
-def __str__(self):
-  return f"ID: {self.__id} | Nome: {self.__nome} | Música: {self.__musica}"
+    def __str__(self):
+        return f"ID: {self.__id} | Nome: {self.__nome} | Música: {self.__musica}"
