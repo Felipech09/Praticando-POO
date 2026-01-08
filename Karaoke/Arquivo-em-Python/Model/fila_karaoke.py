8e93afd851dda30fcd47be488ab640f64087cd2a
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2026-01-07 21:55:39 -0300

    Update fila_karaoke.py
    Apenas um erro de digitação

--- Karaoke/Arquivo-em-Python/Model/fila_karaoke.py
+++ Karaoke/Arquivo-em-Python/Model/fila_karaoke.py
@@ -1,29 +1,29 @@
-calss FilaKaraoke:
-  def __init__ (self):
-self.__fila = [] 
+class FilaKaraoke:
+    def __init__(self):
+        self.__fila = [] 
 
-  def adicionar_pessoa(self, pessoa):
-    self.__fila.append(pessoa)
+    def adicionar_pessoa(self, pessoa):
+        self.__fila.append(pessoa)
 
-def remover_pessoa(self, id_busca):
-  tamanho_inicial = len(self.__fila)
-  self.__fila = [p for p in self.__fila if p.get_id() != id_busca]
-  return len(self.__fila) < tamanho inicial
+    def remover_pessoa(self, id_busca):
+        tamanho_inicial = len(self.__fila)
+        self.__fila = [p for p in self.__fila if p.get_id() != id_busca]
+        return len(self.__fila) < tamanho_inicial
 
-def alterar_musica(self, id_busca, nova_musica):
-  for p in self.__ fila:
-    if p.get_id() == id_busca:
-      p.set_musica(nova_musica)
-      return True
-  return False
+    def alterar_musica(self, id_busca, nova_musica):
+        for p in self.__fila:
+            if p.get_id() == id_busca:
+                p.set_musica(nova_musica)
+                return True
+        return False
 
-def chamar_proxima(self):
-  if not self.id_empty():
-    returrn self.__fila.pop(0)
-  return False
+    def chamar_proxima(self):
+        if not self.is_empty():
+            return self.__fila.pop(0)
+        return False
 
-def is_empty(self):
-  return len(self.__fila) == 0
+    def is_empty(self):
+        return len(self.__fila) == 0
 
-def get_fila(self):
-  return slef.__fila
+    def get_fila(self):
+        return self.__fila
